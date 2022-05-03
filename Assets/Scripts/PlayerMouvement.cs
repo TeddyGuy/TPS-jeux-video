8aa7943fff15632c500742649605b47bee69795d
Teddy Guy <mediteddyguy@gmail.com>
2022-05-03 19:58:59 -0400

    player can jump and has gravity

--- Assets/Scripts/PlayerMouvement.cs
+++ Assets/Scripts/PlayerMouvement.cs
@@ -8,30 +8,59 @@ public class PlayerMouvement : MonoBehaviour
     Rigidbody rb;
     CharacterController controller;
     public float speed = 30f;
+    public float gravity = -9.81f;
+    public float jumpForce = 8f;
+
+    private Vector3 velocity;
+
+    public LayerMask groundLayer;
     Vector3 mouvementDirection;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
         controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetMouvementInput();
+        HandleInput();
+        Move();
+        ApplyGravity();
     }
-
-    private void FixedUpdate()
+    private void HandleInput()
     {
-        Move(mouvementDirection);
+        GetMouvementInput();
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) { Jump(); }
+        }
     }
 
-    private void Move(Vector3 mouvementDirection)
+    private void GetMouvementInput()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        mouvementDirection = (transform.right * x) + (transform.forward * z);
+    }
+
+    private void Move()
     {
         controller.Move(mouvementDirection * speed * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
     }
 
-    private void GetMouvementInput() {
-        mouvementDirection = (transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical"));
+    private void ApplyGravity() {
+        velocity.y = (controller.isGrounded && velocity.y < 0 ? -1f : velocity.y - gravity * -2f * Time.deltaTime ); 
     }
+
+    private void Jump() {
+        Debug.Log("Jump!");
+        velocity.y = jumpForce;
+    }
+
+    
+
+    
+    
 }
